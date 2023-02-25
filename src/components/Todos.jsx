a211419c93ca676c4d90dccb354bfee0f93990f6
John Mason <johnmsn22@gmail.com>
2023-02-25 01:37:49 +0100

    mark button

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -15,20 +15,22 @@ const Todos = ({ todos, deleteTask, setUpdateTodo, markCompleted }) => {
                             <div key={task.id}>
                                 <div className="todos">
                                     <div className="todo-list">
-                                        <div className="todo-item">
+                                        <div className={task.isDone ? 'done' : ''}>
                                             <span className="task-number">{index + 1}</span>
                                             <span className="todo-text">{task.title}</span>
                                             <span className="todo-date">{task.createdAt}</span>
                                         </div>
                                         <div className="icons-wrap">
-                                            <span onClick={(e)=>markCompleted(task.id)} ><i className='fa fa-circle'></i></span>
-                                            <span title='Edit' onClick={() => setUpdateTodo({
-                                                id: task.id,
-                                                title: task.title,
-                                                isDone: task.isDone ? true : false
-                                            })}>
-                                                <i className='fa fa-pen'></i>
-                                            </span>
+                                            <span onClick={(e) => markCompleted(task.id)} ><i className='fa fa-circle'></i></span>
+                                            {task.isDone ? null : (
+                                                <span title='Edit' onClick={() => setUpdateTodo({
+                                                    id: task.id,
+                                                    title: task.title,
+                                                    isDone: task.isDone ? true : false
+                                                })}>
+                                                    <i className='fa fa-pen'></i>
+                                                </span>
+                                            )}
                                             <span title='Delete' onClick={() => deleteTask(task.id)} ><i className='fa fa-trash-can'></i></span>
                                         </div>
                                     </div>
